6378d07553fb9fea3558dc4eee4efde73ca18549
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-02 22:03:10 +0530

    building home page

---
+++ client/src/Post.js
@@ -0,0 +1,21 @@
+export default function Post(){
+    return (
+        <div className="post">
+            <div className="image">
+                <img
+                    src="https://ichef.bbci.co.uk/news/1024/cpsprodpb/5496/live/49fdc940-1d9b-11ef-a090-6b563b2f66e3.jpg.webp"
+                    alt=""/>
+            </div>
+            <div className="texts">
+                <h2>Call of Duty to go to Game Pass in gaming shake-up</h2>
+                <p className="info">
+                    <a className="author">Dawid Paszko</a>
+                    <time>2024-5-29 23:04</time>
+                </p>
+                <p className="summary">Microsoft has torn up the rules of big video game launches by announcing its most
+                    eagerly awaited
+                    new title - Call of Duty: Black Ops 6 </p>
+            </div>
+        </div>
+    );
+}
