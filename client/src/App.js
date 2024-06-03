ffa294a25884f2cae31e78904d28c2e884d08962
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-03 17:16:53 +0530

    Set Routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import Post from "./Post";
+import Header from "./Header";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-      <main>
-        <header>
-          <a href="" className="logo">MyBlog</a>
-          <nav>
-            <a href="">Login</a>
-            <a href="">Register</a>
-          </nav>
-        </header>
-          <Post/>
-          <Post/>
-          <Post/>
-      </main>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<IndexPage />}/>
+            <Route path="/login" element={<LoginPage />}/>
+          </Route>
+      </Routes>
+
   );
 }
 
