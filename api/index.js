0719608b50f1f0d23afd34356815d5e39cc2c2e4
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-06 22:04:45 +0530

    Authentication, function register

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/test',(req,res) =>{
-    res.json('test ok');
+app.post('/register',(req,res) =>{
+    res.json('test ok2');
 });
 app.listen(4000)
