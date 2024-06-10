dfa0c49cf839b9982e87027e01babb6d28d179b7
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-10 22:20:36 +0530

    add mongoose and create usermodel

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors =require('cors');
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 
@@ -10,4 +9,5 @@ app.post('/register',(req,res) =>{
     const {username,password} =req.body;
     res.json({requestData:{username,password}});
 });
-app.listen(4000)
+app.listen(4000);
+//mongodb+srv://dananjayaperera99:XBpgwQFhTNm9wQFZ@cluster0.uhlqqfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
