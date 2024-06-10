0cbacd740d19b82df5f635a38d24dccf51ae50aa
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-10 23:34:20 +0530

    try catch exception

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors =require('cors');
 const mongoose = require("mongoose");
+const User = require('./models/User')
 const app = express();
 
 app.use(cors());
@@ -8,9 +9,17 @@ app.use(express.json());
 
 mongoose.connect('mongodb+srv://dananjayaperera99:tbl0SifpRdrEbLiT@cluster0.uhlqqfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
-app.post('/register',(req,res) =>{
+app.post('/register',async (req,res) =>{
     const {username,password} =req.body;
-    res.json({requestData:{username,password}});
+    try{
+        const userDoc= await User.create({username, password});
+        res.json(userDoc);
+    }catch (e){
+        res.status(400).json(e);
+
+    }
+
+
 });
 app.listen(4000);
 
