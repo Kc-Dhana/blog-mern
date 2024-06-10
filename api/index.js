3a9818cbca294b7098062b1470603d5a40248730
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-10 22:55:19 +0530

    connect momgoose db

--- api/index.js
+++ api/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors =require('cors');
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect('mongodb+srv://dananjayaperera99:tbl0SifpRdrEbLiT@cluster0.uhlqqfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
 app.post('/register',(req,res) =>{
     const {username,password} =req.body;
     res.json({requestData:{username,password}});
 });
 app.listen(4000);
-//mongodb+srv://dananjayaperera99:XBpgwQFhTNm9wQFZ@cluster0.uhlqqfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
