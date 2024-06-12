4fd87d066a45c90ece7668d7cba8836fe02ee21d
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-12 21:18:23 +0530

    add cookie-parser and get username & id after login

--- api/index.js
+++ api/index.js
@@ -5,12 +5,14 @@ const User = require('./models/User')
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
+const cookieParser =require('cookie-parser');
 
 const salt =bcrypt.genSaltSync(10);
 const secret ='asdfe45we45w345wegw345werjktjwertkj';
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://dananjayaperera99:tbl0SifpRdrEbLiT@cluster0.uhlqqfj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
@@ -45,6 +47,13 @@ app.post('/login',async (req,res) => {
     }
 });
 
+app.get('/profile', (req,res) =>{
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) =>{
+        if(err) throw err;
+        res.json(info);
+    });
+});
 
 app.listen(4000);
 
