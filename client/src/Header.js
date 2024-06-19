30a39f5326fa10664b247483a7ebcf1126b5d872
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-19 21:12:54 +0530

    logout function

--- client/src/Header.js
+++ client/src/Header.js
@@ -12,6 +12,16 @@ export default function Header(){
             });
         });
     }, []);
+
+    function logout(){
+        fetch('http://localhost:4000/logout',{
+            credentials:'include',
+            method:'POST',
+        })
+
+    }
+
+
     return (
         <header>
             <Link to="/" className="logo">MyBlog</Link>
@@ -19,7 +29,7 @@ export default function Header(){
                 {username && (
                     <>
                         <Link to="/create">Create new post</Link>
-                        <a>Logout</a>
+                        <a onClick={logout}>Logout</a>
                     </>
                 )}
                 {!username && (
