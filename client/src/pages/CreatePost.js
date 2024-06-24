db8090dc585c310af7196d53cf9fb2719b5540da
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-24 23:33:53 +0530

    create new post

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,6 +1,7 @@
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 import {useState} from "react";
+import {Navigate} from "react-router-dom";
 
 const modules = {
     toolbar: [
@@ -22,8 +23,30 @@ export default function CreatePost(){
     const [title,setTitle] = useState('');
     const [summary,setSummary] = useState('');
     const [content,setContent] = useState('');
+    const [files, setFiles] = useState('');
+    const [redirect, setRedirect] = useState(files);
+    async function createNewPost(ev){
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+
+        ev.preventDefault();
+        const response = await fetch('http://localhost:4000/post', {
+            method:'POST',
+            body: data,
+        });
+        if (response.ok){
+            setRedirect(true);
+        }
+    }
+
+    if(redirect){
+        return <Navigate to={'/'}/>
+    }
     return(
-        <form>
+        <form onSubmit={createNewPost}>
             <input type="title"
                    placeholder={'Title'}
                    value={title}
@@ -32,7 +55,8 @@ export default function CreatePost(){
                    placeholder={'Summary'}
                    value={summary}
                    onChange={ev => setSummary(ev.target.value)}/>
-            <input type="file" />
+            <input type="file"
+                   onChange={ev => setFiles(ev.target.files)}/>
             <ReactQuill
                 value={content}
                 onChange={newValue=> setContent(newValue)}
