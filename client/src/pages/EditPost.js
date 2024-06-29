ed75ae9c3d6b254495e6435c8f08c80307699450
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-29 19:41:16 +0530

    Edit Post page

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,39 +1,25 @@
-import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.snow.css';
 import {useState} from "react";
 import {Navigate} from "react-router-dom";
+import ReactQuill from "react-quill";
 import Editor from "../Editor";
 
-
-export default function CreatePost(){
+export default function EditPost(){
     const [title,setTitle] = useState('');
     const [summary,setSummary] = useState('');
     const [content,setContent] = useState('');
     const [files, setFiles] = useState('');
-    const [redirect, setRedirect] = useState(files);
-    async function createNewPost(ev){
-        const data = new FormData();
-        data.set('title', title);
-        data.set('summary', summary);
-        data.set('content', content);
-        data.set('file', files[0]);
+    const [redirect, setRedirect] = useState('');
 
+    function updatePost(ev){
         ev.preventDefault();
-        const response = await fetch('http://localhost:4000/post', {
-            method:'POST',
-            body: data,
-            credentials: 'include',
-        });
-        if (response.ok){
-            setRedirect(true);
-        }
     }
 
     if(redirect){
         return <Navigate to={'/'}/>
     }
+
     return(
-        <form onSubmit={createNewPost}>
+        <form onSubmit={updatePost}>
             <input type="title"
                    placeholder={'Title'}
                    value={title}
@@ -44,7 +30,7 @@ export default function CreatePost(){
                    onChange={ev => setSummary(ev.target.value)}/>
             <input type="file"
                    onChange={ev => setFiles(ev.target.files)}/>
-            <Editor value={content} onChange={setContent}/>
+            <Editor onChange={setContent} value={content}/>
             <button style={{marginTop:'5px'}}>Create post</button>
         </form>
     );
