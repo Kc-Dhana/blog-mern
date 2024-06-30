fd601e6cd33673953e942afd88f4e65565c82b51
K.C Dhananjaya Perera <dananjayaperera99@gmail.com>
2024-06-30 22:38:44 +0530

    Edit Post page

--- api/index.js
+++ api/index.js
@@ -89,6 +89,17 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res) =>{
         res.json({postDoc});
     });
 
+});
+
+app.put('/post', uploadMiddleware.single('file'), async (req,res) =>{
+    let newPath = null;
+    if(req.file){
+        const {originalname,path} = req.file;
+        const parts = originalname.split('.');
+        const ext =parts[parts.length -1];
+        newPath= path+'.'+ext;
+        fs.renameSync(path,newPath);
+    }
 
 });
 
